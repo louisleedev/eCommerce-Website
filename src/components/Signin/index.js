4e8c34ef12920e717314ca89895c43d7ad1414f8
louisleedev <bj.louislee@gmail.com>
2023-12-09 23:07:43 -0500

    styles modify

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import './styles.scss' 
+import Button from '../forms/Button'
 
 const SignIn = props => {
   return (
     <div className='signin'>
         <div className='wrap'>
             <h2>
-                Login
+                LogIn
             </h2>
+            <div className='formWrap'>
+              <form>
+                <div className='socialSignin'>
+                  <div className='row'>
+                    <Button>
+                      Sign in with Google
+                    </Button>
+                  </div>
+                </div>
+              </form>
+            </div>
         </div>
     </div>
   )
